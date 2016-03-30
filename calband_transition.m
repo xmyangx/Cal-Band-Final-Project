874096287b47d6c09bf3f10650864aabfce77d30
xmyangx <m.yang.123@hotmail.com>
2016-03-29 17:55:19 -0700

    Update calband_transition.m

--- calband_transition.m
+++ calband_transition.m
@@ -1,5 +1,17 @@
 function [instructions] = calband_transition(initial_formation, ...
 target_formation, max_beats)
+%max steps that can be taken; there are 2 beats to a step
 max_steps = max_beats/2;
+%number of marchers
+nb = max(max(initial_formation));
+
+%find the distances;gets a cell of arrays for each person; index # is the
+%tag for each marcher
+[distances,wait_times] = subtraction(initial_fomation,target_formation,max_steps,nb);
+
+%determine optimum placements of the various marchers; collision checker
+%inside the placement function
+
+
 
 end
